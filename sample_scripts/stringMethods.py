2a5d2e1293d4e95abd0e0917c8fd1b62fd80bd16
Fagun Bhavsar <github.fagun@gmail.com>
2019-10-01 16:25:42 +0530

    Robot testcases and other sample scripts

---
+++ sample_scripts/stringMethods.py
@@ -0,0 +1,55 @@
+a = 'python'
+b = 'blog'
+c = 'tutorial'
+d = 'is'
+e = 'awesome'
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+print(a+b)
+
+print(a+b+c+d+e)
+
+print(a+' '+b+' '+c+' '+d+' '+e)
+
+print(a+'\n'+b+'\n'+c+'\n'+d+'\n'+e)
+
+print(a[0])
+
+print(b[1])
+
+print(c[2])
+
+print(d[1])
+
+print(e[5])
+
+print(a[0]+b[1]+c[0]+d[1]+e[5]+c[0])
+
+print(a[0:2])
+
+print(b[1:3])
+
+print(c[::2])
+
+print(a[-7:-4])
+
+print(b[-6:-3])
+
+print(c[-1:-1])
+
+print(c[-1::-1])
+
+print(' %(placeholder)s' %{"placeholder" : "XXXX"})
+
+print(' %(placeholder)s' %{"placeholder" : "XXXX, YYYY, ZZZZ"})
+
+print(' %(integer)01d' %{"integer" :1})
+
+print(' %(integer)02d' %{"integer" :1})
+
+print(' %(integer)03d' %{"integer" :1})
